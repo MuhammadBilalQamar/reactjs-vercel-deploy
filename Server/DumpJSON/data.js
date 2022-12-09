33198cb16774cd4110daf991e264a91de1734329
Muhammad Bilal Qamar <bilalqmr1@gmail.com>
2022-12-09 03:20:54 -0800

    :tada: Code updated

---
+++ Server/DumpJSON/data.js
@@ -0,0 +1,382 @@
+export const Sectors = [
+  {
+    id: 1,
+    label: "Manufacturing",
+    value: "1",
+    subSectors: [
+      {
+        id: 1,
+        label: "Manufacturing_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Manufacturing_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Manufacturing_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Construction materials",
+    value: "19",
+    subSectors: [
+      {
+        id: 1,
+        label: "Construction_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Construction_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Construction_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Electronics and Optics",
+    value: "18",
+    subSectors: [
+      {
+        id: 1,
+        label: "Electronics_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Electronics_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Electronics_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Food and Beverage",
+    value: "6",
+    subSectors: [
+      {
+        id: 1,
+        label: "Food_And_Beverage_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Food_And_Beverage_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Food_And_Beverage_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Bakery & confectionery products",
+    value: "342",
+    subSectors: [
+      {
+        id: 1,
+        label: "Bakery_And_Confectionery_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Bakery_And_Confectionery_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Bakery_And_Confectionery_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Beverages",
+    value: "43",
+    subSectors: [
+      {
+        id: 1,
+        label: "Beverages_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Beverages_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Beverages_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Fish & fish products ",
+    value: "42",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Meat & meat products",
+    value: "40",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Milk & dairy products ",
+    value: "39",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Other",
+    value: "437",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Sweets & snack food",
+    value: "378",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Furniture",
+    value: "13",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Bathroom/sauna ",
+    value: "389",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Bedroom",
+    value: "385",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  {
+    label: "Children’s room ",
+    value: "390",
+    subSectors: [
+      {
+        id: 1,
+        label: "Fish_And_fish_1",
+        value: "1",
+      },
+      {
+        id: 2,
+        label: "Fish_And_fish_2",
+        value: "2",
+      },
+      {
+        id: 3,
+        label: "Fish_And_fish_3",
+        value: "3",
+      },
+    ],
+  },
+  { label: "Kitchen ", value: "98" },
+  { label: "Living room ", value: "101" },
+  { label: "Office", value: "392" },
+  { label: "Other (Furniture)", value: "394" },
+  { label: "Outdoor ", value: "341" },
+  { label: "Project furniture", value: "99" },
+  { label: "Machinery", value: "12" },
+  { label: "Machinery components", value: "94" },
+  { label: "Machinery equipment/tools", value: "91" },
+  { label: "Manufacture of machinery ", value: "224" },
+  { label: "Maritime", value: "97" },
+  { label: "Aluminium and steel workboats ", value: "271" },
+  { label: "Boat/Yacht building", value: "269" },
+  { label: "Ship repair and conversion", value: "230" },
+  { label: "Metal structures", value: "93" },
+  { label: "Other", value: "508" },
+  { label: "Repair and maintenance service", value: "227" },
+  { label: "Metalworking", value: "11" },
+  { label: "Construction of metal structures", value: "67" },
+  { label: "Houses and buildings", value: "263" },
+  { label: "Metal products", value: "267" },
+  { label: "Metal works", value: "542" },
+  { label: "CNC-machining", value: "75" },
+  { label: "Forgings, Fasteners ", value: "62" },
+  { label: "Gas, Plasma, Laser cutting", value: "69" },
+  { label: "MIG, TIG, Aluminum welding", value: "66" },
+  { label: "Plastic and Rubber", value: "9" },
+  { label: "Packaging", value: "54" },
+  { label: "Plastic goods", value: "556" },
+  { label: "Plastic processing technology", value: "559" },
+  { label: "Blowing", value: "55" },
+  { label: "Moulding", value: "57" },
+  { label: "Plastics welding and processing", value: "53" },
+  { label: "Plastic profiles", value: "560" },
+  { label: "Printing ", value: "5" },
+  { label: "Advertising", value: "148" },
+  { label: "Book/Periodicals printing", value: "150" },
+  { label: "Labelling and packaging printing", value: "145" },
+  { label: "Textile and Clothing", value: "7" },
+  { label: "Clothing", value: "44" },
+  { label: "Textile", value: "45" },
+  { label: "Wood", value: "8" },
+  { label: "Other (Wood)", value: "337" },
+  { label: "Wooden building materials", value: "51" },
+  { label: "Wooden houses", value: "47" },
+  { label: "Other", value: "3" },
+  { label: "Creative industries", value: "37" },
+  { label: "Energy technology", value: "29" },
+  { label: "Environment", value: "33" },
+  { label: "Service", value: "2" },
+  { label: "Business services", value: "25" },
+  { label: "Engineering", value: "35" },
+  { label: "Information Technology and Telecommunications", value: "28" },
+  { label: "Data processing, Web portals, E-marketing", value: "581" },
+  { label: "Programming, Consultancy", value: "576" },
+  { label: "Software, Hardware", value: "121" },
+  { label: "Telecommunications", value: "122" },
+  { label: "Tourism", value: "22" },
+  { label: "Translation services", value: "141" },
+  { label: "Transport and Logistics", value: "21" },
+  { label: "Air", value: "111" },
+  { label: "Rail", value: "114" },
+  { label: "Road", value: "112" },
+  { label: "Water", value: "113" },
+];
